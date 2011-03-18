34fa5b00875452f1c6d00fe54b9203cb93d3ddcc
Brian Risinger <brian.risinger@gmail.com>
2011-03-18 00:04:41 -0400

    Several changes to new_comment_marker
    The new comment marker has ben modified to highlight replies to your
    posts in red instead of blue, and a separate count for reples to your
    posts (if any) is displayed next to the filters button/link.
    
    If there are replies to your post, the new count of replies to your
    posts becomes clickable, which opens a 'dialog' (similar to the filters
    'dialog'), with 'quick links' to the replies which if clicked, open
    that post and scrolls to it.
    
    The images used for the connecting lines between a post and it's replies
    have been updated in two ways.  First, they now have a transparent
    background, and the normal line color has been changed to medium gray
    (#808080). This allows them to work better with scripts / styles that
    change the background (the same image is now being used for normal and
    white shack). This also allows them to work with scripts that add a
    background image such as TroZ's ShackChattyNews. This has the side effect
    of making the 'fade out' of the last few pixels to black / white not
    feasable and still be a gif, so these were removed, which allowed the
    image to be implemented with a 2 color palete, which slightly reduced
    their size.
    
    Finally, some improvements were made so the script better handles news
    article pages.  The script now correctly detects the 'See All Comments'
    link in the 'chatty_settings' area and no longer modifies it.
    
    DETECTION OF YOUR POSTS
    This is an important part of the script, as improperly doing this would
    defeat the purpose of this modification.  There are two methods at the
    beginning of th emodified script for accomplishing this, one of which
    is currently setup to be called.  The first method checks local storage
    first for a saved 'user id', and will assume it is correct if found.
    Only if it is not found will it use an XPATH request to search for an
    'a' tag with a 'this_user' class, and look at the parent node's classes
    for the user's id.  The other method not currently being used takes the
    opposite approach, always using an xpath request, and only used the
    stored version if the XPATH come up empty. Both version store the id if
    it is found when using the XPATH query.  The first version operates more
    quickkly, but assumes one shacker per computer. The second (currently
    unused) version is slower, but works better for shackers who sometimes
    share a computer, correctly switching users to highligh after they post
    at least once after logging in.

--- scripts/new_comment_highlighter.js
+++ scripts/new_comment_highlighter.js
@@ -3,6 +3,41 @@ settingsLoadedEvent.addHandler(function()
 {
     if (getSetting("enabled_scripts").contains("new_comment_highlighter"))
     {
+		//uses saved value first, if not set gets it from page
+		function getMyID() {
+			var id = getSetting("my_id", null);		
+			if(id == null || id.trim().length<1) 
+			{ 
+				var elem = document.evaluate('//span[contains(@class,"this_user")]/..', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+				elem = elem.singleNodeValue;
+				if(elem!=null) 
+				{
+					id = elem.className.substr(elem.className.indexOf("olauthor_")+9);
+					setSetting("my_id",id);
+				} 
+			}
+			
+			return id;
+		}
+
+		//gets the user id from the page first, only uses saved version if can't find a post from the user on the page - may work better for shackers who share a computer
+		function getMyID_alt() {
+			var id = null;
+			var elem = document.evaluate('//span[contains(@class,"this_user")]/..', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+			elem = elem.singleNodeValue;
+			if(elem!=null) 
+			{
+				id = elem.className.substr(elem.className.indexOf("olauthor_")+9);
+				setSetting("my_id",id);
+			} 
+			else 
+			{
+				id = getSetting("my_id", null);
+			}
+			
+			return id;
+		}
+		
         NewCommentHighlighter =
         {   
             highlight: function()
@@ -25,32 +60,79 @@ settingsLoadedEvent.addHandler(function()
             highlightPostsAfter: function(last_id)
             {
                 var new_posts = NewCommentHighlighter.getPostsAfter(last_id);
+				var myReplies = new Array();
+				var myid = getMyID();
 
                 var post;
                 for (i = 0; i < new_posts.snapshotLength; i++)
                 {
                     var post = new_posts.snapshotItem(i);
-
-                    // root post
-                    if (post.className == 'sel last')
-                        post.className += ' newcommenthighlighter_newrootpost';
-                    else if (post.className == 'last')
-                        post.className += ' newcommenthighlighter_last';
-                    else if (post.className == '')
-                        post.className += 'newcommenthighlighter';
+					
+					if(NewCommentHighlighter.replyToMe(post,myid))
+					{
+						myReplies.push(post);
+						if (post.className == 'last')
+							post.className += ' newcommenthighlighter_last_tome';
+						else if (post.className == '')
+							post.className += 'newcommenthighlighter_tome';
+					}
+					else
+					{
+						if (post.className == 'sel last')
+							post.className += ' newcommenthighlighter_newrootpost';
+						else if (post.className == 'last')
+							post.className += ' newcommenthighlighter_last';
+						else if (post.className == '')
+							post.className += 'newcommenthighlighter';
+					}
 
                     if (post == post.parentNode.childNodes[1])
                         post.parentNode.className = 'newcommenthighlighter';
                 }
 
-                NewCommentHighlighter.displayNewCommentCount(new_posts.snapshotLength);
+                NewCommentHighlighter.displayNewCommentCount(new_posts.snapshotLength,myReplies);
+				myReplies = null;
             },
 
-            displayNewCommentCount: function(count)
+            displayNewCommentCount: function(count, myReplies)
             {
-					var query = '//div[@id="chatty_settings"]';
-					var cs = document.evaluate(query, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-					cs.innerHTML = cs.innerHTML.replace(' Comments', ' Comments (' + count + ' New)');
+				//find XXX Comments, but not 'Show All Comments'
+				var query = '//div[@id="chatty_settings"]/a';
+				var cs = null;
+				var csall = document.evaluate(query, document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
+				for (i = 0; i < csall.snapshotLength; i++)
+                {
+                    var tmp = csall.snapshotItem(i);
+					if(tmp.innerHTML.indexOf('Comments')>0 && tmp.innerHTML.indexOf('All')<0){
+						cs = tmp;
+					}
+				}
+				
+				if(cs == null){
+					return;
+				}
+				
+				//remove link around 'XXX Comments'
+				var next = cs.nextSibling;
+				var parent = cs.parentNode
+				var text = cs.innerHTML;
+				parent.removeChild(cs);
+				cs = document.createElement('span');
+				parent.insertBefore(cs,next);
+				
+				//update text
+				var str = ' Comments (' + count + ' New';
+				if(myReplies.length>0)
+				{
+					str += ', <a href="#" id="newrepliestoyou"><span class="jt_red">'+myReplies.length+' to you</span></a>';
+				}
+				str += ')';
+				cs.innerHTML = text.replace(' Comments', str); 
+				
+				if(myReplies.length>0)
+				{
+					NewCommentHighlighter.addMyReplies(cs,myReplies);
+				}
             },
 
             getPostsAfter: function(last_id)
@@ -74,7 +156,110 @@ settingsLoadedEvent.addHandler(function()
 
                 var id = post.parentNode.id;
                 return parseInt(id.substr(5));
-            }
+            },
+			
+			replyToMe: function(post, myid)
+			{
+				//determines if a particular reply is to the currently logged in user or not.
+				var tome = false;
+				if(myid != null) {
+					var parentdiv = post.parentNode.parentNode;
+					var parentclass = '';
+					
+					if(parentdiv.className != "root") 
+					{
+						if(parentdiv.className == "capcontainer") 
+						{
+							// parent is a root post
+							parentclass = parentdiv.parentNode.childNodes[1].className;
+						} 
+						else 
+						{
+							// parent is a reply
+							parentclass = parentdiv.childNodes[1].className;
+						}
+											
+						if(parentclass!=null && parentclass.trim().length > 9){
+							//slight improvement by TroZ here to prevent user 12345's posts from being highlighted for user 2345 - was happening between me and tboomer2 under OverloadUT's original version of script
+							parentclasses=parentclass.split(' ');
+							for(var j=parentclasses.length-1;j>=0;j--)
+							{//reverse order as the author is typically the last class
+								if(parentclasses[j].indexOf('fpauthor_')>-1 || parentclasses[j].indexOf('olauthor_')>-1)
+								{
+									parentclass = parentclasses[j];
+									if(parentclass.split('_')[1] == myid) 
+									{
+										tome = true;
+									}
+									break;
+								}
+							}
+						}
+					}
+				}
+				return tome;
+			},
+					
+			addMyReplies: function(cs, myReplies){
+				//adds a list of links to new replies to your posts in a popup 'window'
+				
+//console.log("creating popup dialog "+myReplies);
+//console.log("length "+myReplies.length);
+
+				var div = document.createElement('div');
+				div.setAttribute('class','interiorform hidden');
+				div.setAttribute('id','newrepliestoyouform');
+				var str = '<div class="closeform"><a href="#" id="newrepliestoyouformclose" title="Close Filters">x</a></div>\n';
+				str += '<h2>Replies to your posts</h2><hr>\n';
+				while(myReplies.length>0)
+				{
+					var post = myReplies.pop();
+					var url = '#'+post.getAttribute('id');
+					var link = getDescendentByTagAndClassName(post,'a','shackmsg');
+					var name = getDescendentByTagAndClassName(post,'span','oneline_user');
+					var text = link.textContent.length>3?link.textContent.substring(0,link.textContent.length-3):'';  //remove the ' : ' between post and name
+					str+='<a href="'+url+'"';
+					if(link!=null)
+					{
+						str+=' onclick="'+link.getAttribute('onclick').replace('return','')+'"';
+					}
+					str+='>'+name.innerHTML+': '+text+'</a>\n';
+				}
+				div.innerHTML = str;
+				cs.parentNode.appendChild(div);
+//console.log("added");
+				
+				var node = document.getElementById('newrepliestoyou');
+				if(node != null){ node.addEventListener('click', NewCommentHighlighter.showReplies, true);
+				}else{ console.log(" failed to add newrepliestoyou onclick "); }
+				
+				node = document.getElementById('newrepliestoyouformclose');
+				if(node != null){ node.addEventListener('click', NewCommentHighlighter.hideReplies, true);
+				}else{ console.log(" failed to add newrepliestoyouformclose onclick "); }
+				
+//console.log("done");
+
+			},
+			
+			showReplies: function()
+			{
+console.log("show");
+				var elm = document.getElementById('newrepliestoyouform');
+				if(elm!=null)
+				{
+					elm.className = 'interiorform';
+				}
+			},
+			
+			hideReplies: function()
+			{
+console.log("hide");
+				var elm = document.getElementById('newrepliestoyouform');
+				if(elm!=null)
+				{
+					elm.className += ' hidden';
+				}
+			}
         }
 
         NewCommentHighlighter.highlight();
